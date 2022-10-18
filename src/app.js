03ce24667c694d322f389990e2513fff08fdbccf
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-10-19 08:48:49 +1100

    Create parseDateMs() function and round result
    - Create parseDateMs() function that creates a new Date object (in ms) from a date string
    - Round the result of calculateDelta() to nearest integer

--- src/app.js
+++ src/app.js
@@ -6,6 +6,16 @@ function getTodayMs() {
 
 function calculateDelta(day1, day2) {
     deltaMs = Math.abs(day1 - day2)
-    deltaDays = deltaMs / 86400000
+    deltaDays = Math.round(deltaMs / 86400000)
     return deltaDays
 }
+
+
+function parseDateMs(dateString) {
+    let dateMs = new Date(dateString)
+    return dateMs
+}
+
+
+
+console.log(calculateDelta(getTodayMs(), parseDateMs('2022-09-20 ')))
