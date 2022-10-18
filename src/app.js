9cbec77036b2b1665b08b5929da286457de9c477
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-10-18 13:06:29 +1100

    Refactored into functions
    - +getTodayMs()
    - +calculateDelta()
    - Tested working

--- src/app.js
+++ src/app.js
@@ -1,18 +1,11 @@
-// TODAY IN MILLISECONDS
-const todayMs = Date.now();
-console.log(todayMs);
+function getTodayMs() {
+    todayMs = Date.now()
+    return todayMs
+}
 
-// TOMORROW IN MILLISECONDS
-let tomorrowMs = todayMs + 86400000;
-console.log(tomorrowMs);
 
-// TIME DELTAS (DIFFERENCE)
-let deltaMs = tomorrowMs - todayMs;
-console.log(deltaMs)
-let deltaDays = deltaMs / 86400000
-console.log(deltaDays)
-
-// GETTING THE DIFFERENCE BETWEEN TWO DATE STRINGS IN ABSOLUTE VALUE
-let day1 = new Date('October 18, 2022')
-let day2 = new Date('October 31, 2022')
-console.log(Math.abs(day1 - day2))
+function calculateDelta(day1, day2) {
+    deltaMs = Math.abs(day1 - day2)
+    deltaDays = deltaMs / 86400000
+    return deltaDays
+}
