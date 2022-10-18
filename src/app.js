7a79479932b25e81a09cdb97687e38160408db59
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-10-19 08:54:02 +1100

    Declare all function variables as constants
    - All variables are constants

--- src/app.js
+++ src/app.js
@@ -1,21 +1,20 @@
 function getTodayMs() {
-    todayMs = Date.now()
+    const todayMs = Date.now()
     return todayMs
 }
 
 
 function calculateDelta(day1, day2) {
-    deltaMs = Math.abs(day1 - day2)
-    deltaDays = Math.round(deltaMs / 86400000)
+    const deltaMs = Math.abs(day1 - day2)
+    const deltaDays = Math.round(deltaMs / 86400000)
     return deltaDays
 }
 
 
 function parseDateMs(dateString) {
-    let dateMs = new Date(dateString)
+    const dateMs = new Date(dateString)
     return dateMs
 }
-
 
 
 console.log(calculateDelta(getTodayMs(), parseDateMs('2022-09-20 ')))
