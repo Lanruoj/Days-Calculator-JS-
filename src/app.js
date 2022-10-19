76f2a26adee64582fb1e52adbfd0fa9899ba44cb
Lanruoj <98445539+Lanruoj@users.noreply.github.com>
2022-10-19 11:04:46 +1100

    Convert days to weeks and format message
    - Add calculateDeltaWeeks() to convert to weeks, round to 1 decimal place (at most)
    - Format message with string interpolations from evaluated results

--- src/app.js
+++ src/app.js
@@ -4,17 +4,26 @@ function getTodayMs() {
 }
 
 
-function calculateDelta(day1, day2) {
-    const deltaMs = Math.abs(day1 - day2)
-    const deltaDays = Math.round(deltaMs / 86400000)
-    return deltaDays
-}
-
-
 function parseDateMs(dateString) {
     const dateMs = new Date(dateString)
     return dateMs
 }
 
 
-console.log(calculateDelta(getTodayMs(), parseDateMs('2022-09-20 ')))
+function calculateDeltaDays(day1, day2) {
+    const deltaMs = Math.abs(day1 - day2)
+    const deltaDays = Math.round(deltaMs / 86400000)
+    return deltaDays
+}
+
+
+function calculateDeltaWeeks(deltaDays) {
+    return +(deltaDays / 7).toFixed(1)
+}
+
+
+let daysBetween = calculateDeltaDays(getTodayMs(), parseDateMs('2022-11-13 '))
+let weeksBetween = calculateDeltaWeeks(daysBetween)
+let message = `Days: ${daysBetween}\nWeeks: ${weeksBetween}`
+
+console.log(message)
